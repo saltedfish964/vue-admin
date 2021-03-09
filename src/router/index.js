751c686d73d14f677b3bbb208e61be85518b901d
zengminghong <zengminghong@yinzhilv.com>
2021-03-09 17:33:50 +0800

    添加测试路由

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,42 @@ const routes = [
   {
     path: '/',
     component: Layout,
+    redirect: 'Home',
     children: [
       {
         path: '/home',
         name: 'Home',
         component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+      },
+    ],
+  },
+  {
+    path: '/demo/',
+    component: Layout,
+    children: [
+      {
+        path: 'one',
+        component: {
+          render(h) {
+            return h('h1', '测试1');
+          },
+        },
+      },
+      {
+        path: 'two',
+        component: {
+          render(h) {
+            return h('h1', '测试2');
+          },
+        },
+      },
+      {
+        path: 'three',
+        component: {
+          render(h) {
+            return h('h1', '测试3');
+          },
+        },
       },
     ],
   },

346d0fc511e8b11e6832af2185e1d00df700bd38
zengminghong <zengminghong@yinzhilv.com>
2021-03-09 17:24:36 +0800

    layout 组件抽离，sidebar top-header

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,38 +1,14 @@
 <template>
   <section class="app-wrap">
-    <div
-      class="sidebar"
-      :class="{
-        'sidebar-active': collapse
-      }"
-    >
-      <el-scrollbar wrap-class="scrollbar-wrapper">
-        <Menu
-          :data="menu"
-          class="siderbar-menu"
-          :collapse="collapse"
-        ></Menu>
-      </el-scrollbar>
-    </div>
+    <Sidebar :collapse="collapse"></Sidebar>
     <div
       class="main-container"
       :style="`width: ${!collapse ? 'calc(100vw - 300px)' : 'calc(100vw - 64px)'}`"
     >
-      <div class="header">
-        <div
-          class="menu-ctrl"
-          @click="collapse = !collapse"
-        >
-          <i :class="collapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
-        </div>
-        <div class="info">
-          <el-avatar
-            shape="square"
-            size="medium"
-            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-          ></el-avatar>
-        </div>
-      </div>
+      <TopHeader
+        :collapse="collapse"
+        @on-collapse="onCollapse"
+      ></TopHeader>
       <VTabs
         v-model="editableTabsValue"
         type="border-card"
@@ -48,97 +24,27 @@
           {{item.content}}
         </VTabPane>
       </VTabs>
-      <div>
-        <router-view></router-view>
-      </div>
+      <router-view></router-view>
     </div>
   </section>
 </template>
 
 <script>
-import Menu from '@/components/Menu/index.vue';
 import VTabs from '@/components/VTabs/tabs.vue';
 import VTabPane from '@/components/VTabs/tab-pane.vue';
+import Sidebar from './sidebar.vue';
+import TopHeader from './top-header.vue';
 
 export default {
   components: {
-    Menu,
+    Sidebar,
+    TopHeader,
     VTabs,
     VTabPane,
   },
   data() {
     return {
       collapse: false,
-      menu: [
-        {
-          index: '1',
-          route: '/',
-          disabled: false,
-          name: '首页',
-          icon: 'el-icon-location',
-        },
-        {
-          index: '2',
-          route: '/about',
-          disabled: false,
-          name: '关于',
-          icon: 'el-icon-menu',
-        },
-        {
-          index: '3',
-          route: '/list',
-          disabled: false,
-          name: '总列表',
-          popperClass: 'test-class',
-          icon: 'el-icon-menu',
-          children: [
-            [
-              {
-                name: '下拉分组',
-              },
-              {
-                index: '3-1',
-                route: '/list1',
-                disabled: false,
-                name: '列表-1',
-                icon: 'el-icon-menu',
-                children: [
-                  {
-                    index: '3-1-1',
-                    route: '/list3-1-1',
-                    disabled: false,
-                    name: '列表3-1-1',
-                  },
-                  {
-                    index: '3-2-1',
-                    route: '/list3-2-1',
-                    disabled: false,
-                    name: '列表-3-2-1',
-                  },
-                ],
-              },
-            ],
-            [
-              {
-                name: '下拉分组-2',
-              },
-              {
-                index: '3-2',
-                route: '/list2',
-                disabled: false,
-                name: '列表-2',
-              },
-            ],
-          ],
-        },
-        {
-          index: '4',
-          route: '/other',
-          disabled: false,
-          name: '其他',
-          icon: 'el-icon-menu',
-        },
-      ],
       editableTabsValue: '2',
       editableTabs: [
         {
@@ -190,6 +96,11 @@ export default {
       ],
     };
   },
+  methods: {
+    onCollapse(value) {
+      this.collapse = value;
+    },
+  },
 };
 </script>
 
@@ -200,51 +111,9 @@ export default {
   width: 100vw;
   overflow: hidden;
 }
-.sidebar {
-  transition: all 0.3s;
-  width: 300px;
-  background: #fff;
-  border-right: solid 1px #e6e6e6;
-  .scrollbar-wrapper {
-    height: 100vh;
-  }
-  /deep/.el-scrollbar__wrap {
-    overflow: scroll;
-    overflow-x: hidden;
-  }
-  /deep/.el-menu {
-    border-right: 0;
-  }
-}
-.sidebar-active {
-  width: 64px;
-}
 .main-container {
   position: relative;
   transition: width 0.3s;
   flex: 1;
-  .header {
-    display: flex;
-    height: 64px;
-    background: #fff;
-    box-sizing: border-box;
-    align-items: center;
-    justify-content: space-between;
-    .menu-ctrl {
-      width: 64px;
-      height: 64px;
-      font-size: 38px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;
-      &:hover {
-        background: #f1f1f1;
-      }
-    }
-    .info {
-      padding-right: 15px;
-    }
-  }
 }
 </style>
---
+++ src/layout/sidebar.vue
@@ -0,0 +1,125 @@
+<template>
+  <div
+    class="sidebar"
+    :class="{
+        'sidebar-active': collapse
+      }"
+  >
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <Menu
+        :data="menu"
+        class="siderbar-menu"
+        :collapse="collapse"
+      ></Menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import Menu from '@/components/Menu/index.vue';
+
+export default {
+  props: {
+    collapse: Boolean,
+  },
+  components: {
+    Menu,
+  },
+  data() {
+    return {
+      menu: [
+        {
+          index: '1',
+          route: '/',
+          disabled: false,
+          name: '首页',
+          icon: 'el-icon-location',
+        },
+        {
+          index: '2',
+          route: '/about',
+          disabled: false,
+          name: '关于',
+          icon: 'el-icon-menu',
+        },
+        {
+          index: '3',
+          route: '/list',
+          disabled: false,
+          name: '总列表',
+          popperClass: 'test-class',
+          icon: 'el-icon-menu',
+          children: [
+            [
+              {
+                name: '下拉分组',
+              },
+              {
+                index: '3-1',
+                route: '/list1',
+                disabled: false,
+                name: '列表-1',
+                icon: 'el-icon-menu',
+                children: [
+                  {
+                    index: '3-1-1',
+                    route: '/list3-1-1',
+                    disabled: false,
+                    name: '列表3-1-1',
+                  },
+                  {
+                    index: '3-2-1',
+                    route: '/list3-2-1',
+                    disabled: false,
+                    name: '列表-3-2-1',
+                  },
+                ],
+              },
+            ],
+            [
+              {
+                name: '下拉分组-2',
+              },
+              {
+                index: '3-2',
+                route: '/list2',
+                disabled: false,
+                name: '列表-2',
+              },
+            ],
+          ],
+        },
+        {
+          index: '4',
+          route: '/other',
+          disabled: false,
+          name: '其他',
+          icon: 'el-icon-menu',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.sidebar {
+  transition: all 0.3s;
+  width: 300px;
+  background: #fff;
+  border-right: solid 1px #e6e6e6;
+  .scrollbar-wrapper {
+    height: 100vh;
+  }
+  /deep/.el-scrollbar__wrap {
+    overflow: scroll;
+    overflow-x: hidden;
+  }
+  /deep/.el-menu {
+    border-right: 0;
+  }
+}
+.sidebar-active {
+  width: 64px;
+}
+</style>

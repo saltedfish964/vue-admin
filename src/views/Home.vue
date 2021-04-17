d3c3ccdcdfb7e8fec2930b22b3ccee958a36f77b
zengminghong <1041288269@qq.com>
2021-04-17 14:13:21 +0800

    fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,12 @@
 <template>
-  <div class="home">首页<button @click="addNewMenu">添加菜单</button></div>
+  <h1>Home</h1>
 </template>
 
 <script>
-import { resetRouter } from '@/router/index';
-
 export default {
-  name: 'Home',
-  data() {
-    return {
-    };
-  },
-  methods: {
-    addNewMenu() {
-      // resetRouter();
-      console.log(resetRouter);
-      resetRouter();
-      this.$router.push({ name: '404' });
-      // this.$store.commit('menu/addMenu', {
-      //   index: 'Other',
-      //   route: '/other',
-      //   disabled: false,
-      //   name: '其他',
-      //   icon: 'el-icon-location',
-      // });
-    },
-  },
+
 };
 </script>
+
+<style>
+</style>
